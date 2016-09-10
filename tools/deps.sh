24597d09cf53439da14081fa696be164d7cb02e7
elieltavares <elinkpark@gmail.com>
2016-09-10 19:02:16 -0300

    Update deps.sh

--- tools/deps.sh
+++ tools/deps.sh
@@ -1,5 +1,6 @@
-sudo apt-get install libsdl2-dev
-sudo apt-get install libsdl2-image-dev
-sudo apt-get install libsdl2-ttf-dev
-sudo apt get install libsdl2-mixer-dev
-sudo apt-get install libsdl2-dev
+sudo apt-get install g++ -y
+sudo apt-get install libsdl2-dev -y
+sudo apt-get install libsdl2-image-dev -y
+sudo apt-get install libsdl2-ttf-dev -y
+sudo apt get install libsdl2-mixer-dev -y
+sudo apt-get install libsdl2-dev -y
