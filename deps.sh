64b37fdc6e15981b336b6681b0ece25ac809b1fe
elieltavares <elinkpark@gmail.com>
2016-09-10 18:46:03 -0300

    Delete deps.sh

--- deps.sh
+++
@@ -1,5 +0,0 @@
-sudo apt-get install libsdl2-dev
-sudo apt-get install libsdl2-image-dev
-sudo apt-get install libsdl2-ttf-dev
-sudo apt get install libsdl2-mixer-dev
-sudo apt-get install libsdl2-dev
